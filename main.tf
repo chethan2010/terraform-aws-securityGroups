2a85a5a18e92670a6a45511eb6cf9a872509eaf6
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-15 19:17:21 +0530

    expense-sg

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "allow_tls" {
   dynamic "ingress" {
     for_each = var.ingress_rules
     content {
-        from_port       = ingress.value["from_port"]
+        from_port         = ingress.value["from_port"]
         to_port         = ingress.value["to_port"]
         protocol        = ingress.value["protocol"]
         cidr_blocks = ingress.value["cidr_blocks"]
