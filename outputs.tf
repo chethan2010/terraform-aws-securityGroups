40b76afaaa4b9f17e5e62bf5d0f54c81af41a224
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-28 15:50:48 +0530

    expense-sg

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "sg_id" {
   value = aws_security_group.allow_tls.id
 }
+
+output "security_group_ids" {
+  value = aws_security_group.this[*].id
+}
+
+output "sg_id" {
+  value = aws_security_group.allow_tls.id
+}
